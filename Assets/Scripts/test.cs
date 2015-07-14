c82d636a1c2247261907e2a792df247a3bd5d5ce
jane22th <jane22th@gmail.com>
2015-07-14 12:04:39 +0900

    Project_SSL 2015/07/13

---
+++ Assets/Scripts/test.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class test : MonoBehaviour {
+	int flag = 0;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(flag == 0)
+		transform.Translate (new Vector3(0, -0.008f, -0.008f));
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		Debug.Log ("!enter");
+		flag = 1;
+	}
+
+	void OnTriggerStay2D(Collider2D other)
+	{
+		Debug.Log ("stay!");
+	}
+	
+	void OnTriggerExit2D(Collider2D other)
+	{
+		Debug.Log ("exit");
+	}
+}
