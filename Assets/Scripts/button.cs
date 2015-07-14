69f0a06fefa3955ac3a96d5d6962f5cb8a56281e
yh0413 <parkyh93@naver.com>
2015-07-14 21:54:55 +0900

    Revert "Merge remote-tracking branch 'origin/master'"
    This reverts commit e257e606161c884e2565edede9e76288d8bdd53f, reversing
    changes made to 4ac6bd4d1a79f689bc6dc793b1e4a4bb567f50dc.

--- Assets/Scripts/button.cs
+++ Assets/Scripts/button.cs
@@ -4,15 +4,11 @@ using System.Collections;
 public class button : MonoBehaviour {
 
 	void OnMouseDown(){
-<<<<<<< HEAD
 		Debug.Log ("slrkskfzz");
-=======
-		Debug.Log ("zzslrkskf");
->>>>>>> origin/master
 	}
 	
 	// Update is called once per frame
 	void Update () {
-
+	
 	}
 }
